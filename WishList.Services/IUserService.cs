a40453afabfe38d71d8b952edfb786427a4ac675
Johan Driessen <johan@driessen.se>
2011-11-18 22:44:05 +0100

    sm?fix

--- WishList.Services/IUserService.cs
+++ WishList.Services/IUserService.cs
@@ -9,14 +9,19 @@ namespace WishList.Services
 		User CreateUser( User user );
 		void DeleteUser( string username );
 		Guid? GetApprovalTicket( string username );
+
+
 		User GetUser( int userId );
 		User GetUser( string username );
 		IList<User> GetUsers();
-		IList<User> GetFriends( User user );
-		void AddFriend( string username, string friendname);
-		void RemoveFriend( string username, string friendname);
+		
         void UpdatePassword( string username, string newPassword );
 		User UpdateUser( User user );
 		bool ValidateUser( string userName, string password );
+
+		//FriendService
+		IList<User> GetFriends( User user );
+		void AddFriend( string username, string friendname );
+		void RemoveFriend( string username, string friendname );
 	}
 }
